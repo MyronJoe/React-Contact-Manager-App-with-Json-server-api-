66d94f13d2e7bfe896da0086c751e822dd4bbaaa
Myron <josephonyekachi77@gmail.com>
2022-09-25 09:39:27 -0800

    add a json-server api for storage

--- React-contacts-manager/src/index.js
+++ React-contacts-manager/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../src/components/App.js';
+import App from './components/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
