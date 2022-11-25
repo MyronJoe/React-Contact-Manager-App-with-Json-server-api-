66421b5bddb38fc40f33055b9f4488e0975d1828
Myron <josephonyekachi77@gmail.com>
2022-11-24 19:53:27 -0800

    updated

--- React-contacts-manager/src/components/ContactList.jsx
+++ React-contacts-manager/src/components/ContactList.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 import ContactCard from './ContactCard';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function ContactList(props) {
   // console.log(props)
 
-    const delteContactHandler = (id) =>{
-      props.getContactId(id)
-    };
+  const delteContactHandler = (id) => {
+    props.getContactId(id)
+  };
 
-    
+
   const renderContactList = props.contacts.map((contact) => {
-    return(
-      
-        
-        <ContactCard
+    return (
+
+
+      <ContactCard
         contact={contact}
         clickHandler={delteContactHandler}
-        key={contact.id}/>
-      
+        key={contact.id} />
+
     );
   })
 
   return (
     <div className='main'>
       <h1 className='head'>Contact List
-      
-      <Link to='/add'><button className='ui button blue right'>Add Contact</button></Link>
-      
+
+        <Link to='/add'><button className='ui button blue right'>Add Contact</button></Link>
+
       </h1>
 
       <div className='ui celled list'>{renderContactList}</div>
     </div>
-    
+
   )
 };
 
