4fd064f8328482af937890690c8c6134da701462
Myron <josephonyekachi77@gmail.com>
2022-11-24 19:52:21 -0800

    updated

--- React-contacts-manager/src/components/ContactCard.jsx
+++ React-contacts-manager/src/components/ContactCard.jsx
@@ -4,32 +4,33 @@ import user from '../images/user.jpg'
 
 function ContactCard(props) {
   console.log(props.contact)
-  const {id, name, email} = props.contact;
+  const { id, name, email } = props.contact;
   return (
-    
+
     <div className="item1">
-      
+
       <div className="content">
-      <img src={user} alt="user" className='ui avatar image'/>
+        <img src={user} alt="user" className='ui avatar image' />
         <div>
-        <Link to={{
-          pathname:`/contact/${id}`,
-          state:{contact: props.contact}}}>
-          <div className="header">{name}</div>
-          <div>{email}</div>
-        </Link>
-        {/* <div className='id'>{id}</div> */}
+          <Link to={{
+            pathname: `/contact/${id}`,
+            state: { contact: props.contact }
+          }}>
+            <div className="header">{name}</div>
+            <div>{email}</div>
+          </Link>
+          {/* <div className='id'>{id}</div> */}
         </div>
       </div>
       <div>
-      <Link to={{pathname:`/edit/${id}`, state:{contact: props.contact}}}>
-        <i className='edit alternate outline icon icon2'
+        <Link to={{ pathname: `/edit/${id}`, state: { contact: props.contact } }}>
+          <i className='edit alternate outline icon icon2'
+          ></i>
+        </Link>
+
+        <i className='trash alternate outline icon'
+          onClick={() => props.clickHandler(id)}
         ></i>
-      </Link>
-      
-      <i className='trash alternate outline icon'
-        onClick={() => props.clickHandler(id)}
-      ></i>
       </div>
     </div>
   )
