54eb6cda021751dacbbfdb8abbf9f1488dba3dd2
Myron <josephonyekachi77@gmail.com>
2022-11-24 19:52:56 -0800

    updated

--- React-contacts-manager/src/components/ContactDetail.jsx
+++ React-contacts-manager/src/components/ContactDetail.jsx
@@ -3,29 +3,29 @@ import user from '../images/user.jpg'
 import { Link } from 'react-router-dom'
 
 function ContactDetail(props) {
-  return (
-    <div>
+    return (
+        <div>
 
-        <h1 className='head'>User Profile
-            
-            <Link to='/'><button className='ui button blue right'>Home</button></Link>
-      
-        </h1>
-        
-        <div className="main">
-            <div className="ui card centered">
-                <div className="image">
-                    <img src={user} alt="user" />
-                </div>
-                <div className="con">
-                    <h1>Myron</h1> 
-                    <p>Joem@gmail.com</p>
+            <h1 className='head'>User Profile
+
+                <Link to='/'><button className='ui button blue right'>Home</button></Link>
+
+            </h1>
+
+            <div className="main">
+                <div className="ui card centered">
+                    <div className="image">
+                        <img src={user} alt="user" />
+                    </div>
+                    <div className="con">
+                        <h1>Myron</h1>
+                        <p>Joem@gmail.com</p>
+                    </div>
                 </div>
             </div>
+
         </div>
-       
-    </div>
-  )
+    )
 }
 
 export default ContactDetail
